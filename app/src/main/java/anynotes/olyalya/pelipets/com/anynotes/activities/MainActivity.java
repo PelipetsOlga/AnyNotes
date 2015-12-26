d402a14c699d90ee06107e6ae90b3b52d08db58f
Olga Pelipets <pelipets.olga.developer@gmail.com>
2015-12-26 17:50:44 +0200

    refactoring

--- app/src/main/java/anynotes/olyalya/pelipets/com/anynotes/activities/MainActivity.java
+++ app/src/main/java/anynotes/olyalya/pelipets/com/anynotes/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package anynotes.olyalya.pelipets.com.anynotes;
+package anynotes.olyalya.pelipets.com.anynotes.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import anynotes.olyalya.pelipets.com.anynotes.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
