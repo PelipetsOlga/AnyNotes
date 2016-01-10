e8329c4ea2c121a7dab689540b86b6c520925769
Olga Pelipets <pelipets.olga.developer@gmail.com>
2016-01-10 11:48:53 +0200

    repeat alarm grid

--- app/src/main/java/anynotes/olyalya/pelipets/com/anynotes/activities/ReminderActivity.java
+++ app/src/main/java/anynotes/olyalya/pelipets/com/anynotes/activities/ReminderActivity.java
@@ -11,6 +11,8 @@ import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 import anynotes.olyalya.pelipets.com.anynotes.R;
 import anynotes.olyalya.pelipets.com.anynotes.utils.Constants;
 import anynotes.olyalya.pelipets.com.anynotes.utils.NoteUtils;
@@ -24,6 +26,18 @@ public class ReminderActivity extends AppCompatActivity {
     private TextView tvRepeat;
     private LinearLayout gridRepeat;
     private int size;
+    private HashMap<TextView, RepeatTumbler> listRepeat;
+    private TextView lastRepeat;
+
+    private class RepeatTumbler {
+        boolean isChecked;
+        long repeatTime;
+
+        public RepeatTumbler(boolean isChecked, long repeatTime) {
+            this.isChecked = isChecked;
+            this.repeatTime = repeatTime;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +46,6 @@ public class ReminderActivity extends AppCompatActivity {
 
         loadSettings();
         initViews();
-
 
 
     }
@@ -83,10 +96,16 @@ public class ReminderActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     tvRepeat.setTextColor(getResources().getColor(R.color.colorPrimary));
-                     gridRepeat.setVisibility(View.VISIBLE);
+                    gridRepeat.setVisibility(View.VISIBLE);
                 } else {
                     tvRepeat.setTextColor(getResources().getColor(R.color.grayInactive));
                     gridRepeat.setVisibility(View.GONE);
+                    if (lastRepeat != null || listRepeat != null) {
+                        lastRepeat.setBackgroundDrawable(ReminderActivity.this.getResources().getDrawable(R.drawable.big_circle_inactive));
+                        lastRepeat.setTextColor(ReminderActivity.this.getResources().getColor(R.color.grayInactive));
+                        listRepeat.get(lastRepeat).isChecked = false;
+                    }
+                    lastRepeat = null;
                 }
             }
         });
@@ -105,6 +124,50 @@ public class ReminderActivity extends AppCompatActivity {
             }
         });
 
+        listRepeat = new HashMap<TextView, RepeatTumbler>();
+        listRepeat.put((TextView) findViewById(R.id.repeat_1hour), new RepeatTumbler(false, (long) (60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_3hour), new RepeatTumbler(false, (long) (3 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_6hour), new RepeatTumbler(false, (long) (6 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_12hour), new RepeatTumbler(false, (long) (12 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_1day), new RepeatTumbler(false, (long) (24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_2day), new RepeatTumbler(false, (long) (2 * 24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_3day), new RepeatTumbler(false, (long) (3 * 24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_10day), new RepeatTumbler(false, (long) (10 * 24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_1week), new RepeatTumbler(false, (long) (7 * 24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_2week), new RepeatTumbler(false, (long) (2 * 7 * 24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_3week), new RepeatTumbler(false, (long) (3 * 7 * 24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_4week), new RepeatTumbler(false, (long) (4 * 7 * 24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_1month), new RepeatTumbler(false, (long) (30 * 24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_3month), new RepeatTumbler(false, (long) (3 * 30 * 24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_6month), new RepeatTumbler(false, (long) (6 * 30 * 24 * 60 * 60 * 1000)));
+        listRepeat.put((TextView) findViewById(R.id.repeat_year), new RepeatTumbler(false, (long) (365 * 24 * 60 * 60 * 1000)));
+        for (TextView tv : listRepeat.keySet()) {
+            tv.setOnClickListener(new RepeatTumblerListener());
+        }
+    }
+
+    class RepeatTumblerListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            TextView tv = (TextView) v;
+            RepeatTumbler tumbler = listRepeat.get(tv);
+            if (tumbler.isChecked) {
+                tumbler.isChecked = false;
+                tv.setBackgroundDrawable(ReminderActivity.this.getResources().getDrawable(R.drawable.big_circle_inactive));
+                tv.setTextColor(ReminderActivity.this.getResources().getColor(R.color.grayInactive));
+                lastRepeat = tv;
+            } else {
+                tumbler.isChecked = true;
+                tv.setBackgroundDrawable(ReminderActivity.this.getResources().getDrawable(R.drawable.big_circle_active));
+                tv.setTextColor(ReminderActivity.this.getResources().getColor(R.color.colorPrimary));
+                if (lastRepeat != null) {
+                    lastRepeat.setBackgroundDrawable(ReminderActivity.this.getResources().getDrawable(R.drawable.big_circle_inactive));
+                    lastRepeat.setTextColor(ReminderActivity.this.getResources().getColor(R.color.grayInactive));
+                    listRepeat.get(lastRepeat).isChecked = false;
+                }
+                lastRepeat = tv;
+            }
+        }
     }
 
     @Override
